0efe39a0d4632a1cc7b39db269a3ed98259d4544
klesa <kittycaravan@gmail.com>
2018-03-12 16:15:57 +0900

    프로젝트 다시 시작. 집pc에는 그전 소스 없었음.. 깃허브에 올림 맵파일 두개 추가함 스피너로 맵 선택 Glide 적용함 4버전 디자인 대충함

--- app/src/main/java/com/peisia/bgom/ActivityHome.java
+++ app/src/main/java/com/peisia/bgom/ActivityHome.java
@@ -1,13 +1,57 @@
 package com.peisia.bgom;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityHome extends AppCompatActivity {
+
+    private Spinner mSpinnerMapSelector;
+    private ImageView mImageViewFullMap;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mContext = this;
+
+        initView();
+    }
+
+    private void initView() {
+        mImageViewFullMap = (ImageView)findViewById(R.id.imageViewFullMap);
+
+        mSpinnerMapSelector = (Spinner)findViewById(R.id.spinnerMapSelector);
+        mSpinnerMapSelector.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i){
+                    case 0:
+                        Glide.with(mContext).load(R.drawable.map_full_erangel).into(mImageViewFullMap);
+                        break;
+                    case 1:
+                        Glide.with(mContext).load(R.drawable.map_full_miramar).into(mImageViewFullMap);
+                        break;
+                    default:
+                        Glide.with(mContext).load(R.drawable.map_full_erangel).into(mImageViewFullMap);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        mSpinnerMapSelector.setSelection(0);
     }
 }
